fb14a78080f63fe3e50ef94a33d5255803c2cd4e
BehroozGharleghi <gharleghi.bn@gmail.com>
2020-09-22 09:20:05 +0200

    udpate SQL solutions

--- Labs/module-1_labs/03_mysql/lab 4 sql aggregations.sql
+++ Labs/module-1_labs/03_mysql/lab 4 sql aggregations.sql
@@ -59,12 +59,31 @@ select account_id, sum(amount) from bank.loan group by account_id order by sum(a
 # 4. From the loan table, retrieve the number of loans issued for each day, before (excl) 930907, ordered by date in 
 # descending order:
 SELECT * FROM bank.loan;
-SELECT loan.date, COUNT(loan.loan_id) FROM bank.loan WHERE date<930907 GROUP BY date ORDER BY date DESC; 
+SELECT date, COUNT(loan_id) FROM bank.loan WHERE date<930907 GROUP BY date ORDER BY date DESC; 
 
 # 5. From the loan table, for each day in December 1997, count the number of loans issued for each unique loan duration, 
 # ordered by date and duration, both in ascending order. You can ignore days without any loans in your output.
 
-SELECT 
+SELECT duration, COUNT(loan_id), date FROM bank.loan WHERE date BETWEEN 971201 AND 971231 GROUP BY date 
+order by duration ASC; 
+
+# 6. From the trans table, for account_id 396, sum the amount of transactions for each type 
+# (VYDAJ = Outgoing, PRIJEM = Incoming). Your output should have the account_id, the type and the sum of amount, named as
+# total_amount. Sort alphabetically by type.
+SELECT * FROM bank.trans;
+
+SELECT account_id, type, SUM(amount) FROM bank.trans WHERE type = 'VYDAJ' and account_id =396 
+or type = 'PRIJEM' and account_id =396 group by type order by type;
+
+# 7. From the previous output, translate the values for type to English, rename the column to transaction_type, 
+# round total_amount down to an integer
+
+SELECT account_id, type as transaction_type, SUM(amount) FROM bank.trans WHERE type = 'VYDAJ' as 'outgoing' and account_id =396 
+or type = 'PRIJEM' as 'incoming'and account_id =396 group by type order by type;
+
+
+
+
 
 
 
